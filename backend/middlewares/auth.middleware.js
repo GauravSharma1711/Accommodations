4d026322e11b2b539e4f0802b75f8c7969e37a71
Gaurav Sharma <gau1711sha@gmail.com>
2025-04-26 17:58:47 +0530

    added auth middleware

---
+++ backend/middlewares/auth.middleware.js
@@ -0,0 +1,40 @@
+import jwt from 'jsonwebtoken'
+import {ApiError} from '../utils/api-error.js'
+import {ApiResponse} from '../utils/api-response.js'
+import User from '../models/auth.model.js';
+
+export const authMiddleware = async(req,res,next)=>{
+
+    try {
+        const token = req.cookies?.jwt;
+        if(!token){
+            throw new ApiError(401,"token not found")
+        }
+
+
+        const decoded = jwt.verify(token,process.env.JWT_SECRET);
+       if(!decoded){
+        throw new ApiError(403,"unauthorized")
+       }
+
+       const userId = decoded.id;
+
+       const user = await User.findById(userId);
+
+
+       if (!user) {
+        throw new ApiError(404, "User not found");
+      }
+
+       req.user =  user;
+       next()
+
+
+
+    } catch (error) {
+        console.error("Error in auth middleware:", error.message);
+        throw new ApiError(401, "Unauthorized: Invalid token");
+
+    }
+
+}
