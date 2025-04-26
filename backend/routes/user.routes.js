47924bc9d4f566b4a3b8ea2531bd2539071dfa97
Gaurav Sharma <gau1711sha@gmail.com>
2025-04-27 01:12:57 +0530

    user routes written

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,7 +1,13 @@
 
 import express from 'express'
+import { deleteUser, getUser, getUsers, updateUser } from '../controllers/user.controller.js';
 
 const router = express.Router();
+
+router.get('/getUsers',getUsers);
+router.get('/getUser/:id',getUser);
+router.put('/updateUser/:id',updateUser);
+router.delete('/deleteUser/:id',deleteUser);
 
 
 export default router;
