f17fa1dfe1a6c22a73ff3c4c68353043f999d6fd
Gaurav Sharma <gau1711sha@gmail.com>
2025-04-28 17:35:11 +0530

    basic backend setup complete

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,6 +1,6 @@
 
 import express from 'express'
-import { deleteUser, getUser, getUsers, updateUser } from '../controllers/user.controller.js';
+import { deleteUser, getUser, getUsers, profilePost, savePost, updateUser } from '../controllers/user.controller.js';
 import { authMiddleware } from '../middlewares/auth.middleware.js';
 
 const router = express.Router();
@@ -9,6 +9,7 @@ router.get('/getUsers',getUsers);
 router.get('/getUser/:id',getUser);
 router.put('/updateUser/:id',authMiddleware,updateUser);
 router.delete('/deleteUser/:id',deleteUser);
-
+router.post('/savePost',authMiddleware,savePost);
+router.get('/profilePost/:id',authMiddleware,profilePost);
 
 export default router;
