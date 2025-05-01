aa99dfbe0afc2eaed220267a8e3a73e2e0821272
Gaurav Sharma <gau1711sha@gmail.com>
2025-05-01 13:29:12 +0530

    react-router-dom setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,23 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import HomePage from './pages/HomePage'
+import ListPage from './pages/ListPage'
+import LoginPage from './pages/LoginPage'
+import SignUpPage from './pages/SignUpPage'
+import SinglePage from './pages/SinglePage'
+import { Route,Routes } from 'react-router-dom'
 
 const App = () => {
   return (
-    <div
-      className='min-h-screen max-w-7xl mx-auto px-4 sm:px-6 lg:px-8'
-    >
+    <div className='min-h-screen max-w-7xl mx-auto px-4 sm:px-6 lg:px-8' >
       <Navbar />
-      <HomePage />
+      <Routes>
+<Route path="/" element={<HomePage/>} />
+<Route path="/list" element={<ListPage/>} />
+<Route path="/signup" element={<SignUpPage/>} />
+<Route path="/login" element={<LoginPage/>} />
+<Route path="/:id" element={<SinglePage/>} />
+      </Routes>
     </div>
   )
 }
--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -1,15 +1,29 @@
 // components/SearchBar.js
-import React from 'react'
+import React, { useState } from 'react'
 
 
 const SearchBar = () => {
+  const [query, setQuery] = useState({
+    type:"Buy",
+    location:"",
+    minPrice:0,
+    maxPrice:0,
+  })
   return (
     <div className='w-full md:w-4/5 lg:w-3/4 bg-white rounded-lg shadow-md p-4 md:p-6'>
       <div className='flex space-x-2 mb-4'>
-        <button className='h-10 md:h-12 px-4 md:px-6 bg-amber-500 text-white rounded-md font-semibold hover:bg-amber-600 focus:outline-none focus:ring-2 focus:ring-amber-400 focus:ring-offset-1'>
+        <button 
+        className={`h-10 md:h-12 px-4 md:px-6 ${query.type==="Buy" ?"bg-amber-500":"bg-gray-500"}  text-white rounded-md font-semibold hover:bg-amber-600 focus:outline-none focus:ring-2 focus:ring-amber-400 focus:ring-offset-1`}
+        onClick={()=> setQuery(prev =>({...prev,type:"Buy"}))}
+        >
           Buy
         </button>
-        <button className='h-10 md:h-12 px-4 md:px-6 bg-gray-300 text-gray-700 rounded-md font-semibold hover:bg-gray-400 focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-offset-1'>
+        <button 
+        className={`h-10 md:h-12 px-4 md:px-6 ${query.type==="Rent" ?"bg-amber-500":"bg-gray-500"} text-white  rounded-md font-semibold hover:bg-gray-400 focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-offset-1`}
+        
+        onClick={()=> setQuery(prev =>({...prev,type:"Rent"}))}
+        >
+          
           Rent
         </button>
       </div>
@@ -23,14 +37,18 @@ const SearchBar = () => {
         </div>
         <div className='relative flex-grow focus-within:z-10 mb-2 md:mb-0 md:mr-2'>
           <input
-            type='text'
+            type='Number'
+            min={0}
+            max={100000}
             className='block w-full h-10 md:h-12 pl-3 pr-3 sm:text-sm border border-gray-300 rounded-md focus:outline-none focus:ring-amber-500 focus:border-amber-500'
             placeholder='Min Price'
           />
         </div>
         <div className='relative flex-grow focus-within:z-10 mb-2 md:mb-0 md:mr-2'>
           <input
-            type='text'
+            type='Number'
+            min={0}
+            max={100000}
             className='block w-full h-10 md:h-12 pl-3 pr-3 sm:text-sm border border-gray-300 rounded-md md:rounded-r-md focus:outline-none focus:ring-amber-500 focus:border-amber-500'
             placeholder='Max Price'
           />
